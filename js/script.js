5f86b7b70ed8016a714ba3207572b30bbc6e5b5a
Nathan <nschpro@gmail.com>
2022-05-13 12:52:49 +0200

    add some features
    press ENTER  to add
    value to "" when add item

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+input = document.getElementById('input');
+
+input.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("send").click();
+    }
+  });
+
 function add(){
     var element = document.getElementById('input').value;
     var i = 1;
@@ -8,6 +20,7 @@ function add(){
     {
         var p = document.createElement('p');
         p.textContent = element;
+        document.getElementById('input').value = "";
 
         task.appendChild(p,task);
     }
